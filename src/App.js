0be71e34f52db99f766174d2090852f8528d2c94
yahyashanaah <yahya.shanaah@gmail.com>
2025-05-06 14:15:46 +0400

    rendering list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import Post from './Post';
 import SideMenu from './SideMenu';
 
 const showCategory = true;
+
+const listOfPoests = [
+  {
+    id : 1,
+    postName: "Post 1",
+    postType: "This is the content of post 1",
+  },
+  {
+    id : 2,
+    postName: "Post 2",
+    postType: "This is the content of post 2",
+  },
+  {
+    id : 3,
+    postName: "Post 3",
+    postType: "This is the content of post 3",
+  },
+  {
+    id : 4,
+    postName: "Post 4",
+    postType: "This is the content of post 4",
+  },
+]
 function App() {
   return (
     <div className="App">
@@ -13,21 +36,10 @@ function App() {
           style={{ width: "60%", display: "flex", justifyContent: "center" }}
         >
           <div style={{ width: "70%" }}>
-            <Post postName="Yahya Shanaah" postType="Mahmoud Shanaah">
-              <p>
-                Yahya Shanaah is a software engineer with a passion for web
-                development and a knack for problem-solving. He enjoys creating
-                user-friendly applications and is always eager to learn new
-                technologies.
-              </p>
-            </Post>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            {listOfPoests.map((post) => (
+              <Post key={post.id} postName={post.postName} postType={post.postType} />
+            ))}
           </div>
-
           <div style={{ width: "30%" }}>
             <AppSideMenu />
           </div>
---
+++ src/myButton.js
@@ -0,0 +1,16 @@
+export  default function MyButton() {
+    return (
+        <button
+        style={{
+            backgroundColor: "blue",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+        }}
+        >
+        Click Me!
+        </button>
+    );
+}
